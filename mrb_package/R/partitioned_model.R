eace41672d32fa2d449dcaae42cd3a12215fea97
Edie <ehze@live.com>
2018-05-25 13:08:31 -0700

    more friday gigs

--- mrb_package/R/partitioned_model.R
+++ mrb_package/R/partitioned_model.R
@@ -5,6 +5,102 @@
 #' @keywords mrRevBayes
 #' @export
 
+jc_model_2 = function(ix) {
+  return("")
+}
+
+
+#' func_name()
+#'
+#' Insert description
+#' @param param_1 Insert description
+#' @keywords mrRevBayes
+#' @export
+
+k80_model_2 = function(ix) {
+  kappa_1 = "kappa ~ dnExp(1)"
+  kappa_2 = "moves[++move_index] = mvScale(kappa, weight=1.0)"
+  
+  kappa_1 = gsub("kappa", paste0("kappa_",ix), kappa_1)
+  kappa_2 = gsub("kappa", paste0("kappa_",ix), kappa_2)
+  
+  parameters = c(kappa_1, kappa_2)
+  return(parameters)
+}
+
+#' func_name()
+#'
+#' Insert description
+#' @param param_1 Insert description
+#' @keywords mrRevBayes
+#' @export
+
+hky_model_2 = function(ix) {
+  pi_1    = "pi ~ dnDirichlet(v(1,1,1,1))"
+  pi_2    = "moves[++move_index] = mvBetaSimplex(pi, weight=1.0)"
+  kappa_1 = "kappa ~ dnLognormal(0.0, 1.25)"
+  kappa_2 = "moves[++move_index] = mvScale(kappa)"
+  
+  pi_1 = gsub("pi", paste0("pi_",ix), pi_1)
+  pi_2 = gsub("pi", paste0("pi_",ix), pi_2)
+  kappa_1 = gsub("kappa", paste0("kappa_",ix), kappa_1)
+  kappa_2 = gsub("kappa", paste0("kappa_",ix), kappa_2)
+  
+  parameters = c(pi_1, pi_2, kappa_1, kappa_2)
+  return(parameters)
+}
+
+
+#' func_name()
+#'
+#' Insert description
+#' @param param_1 Insert description
+#' @keywords mrRevBayes
+#' @export
+
+sym_model_2 = function(ix) {
+  pi_1 = "pi <- simplex(1,1,1,1)"
+  er_1 = "er ~ dnDirichlet(v(1,1,1,1,1,1))"
+  er_2 = "moves[++move_index] = mvBetaSimplex(er, weight=1.0)"
+  
+  pi_1 = gsub("pi", paste0("pi", ix), pi_1)
+  er_1 = gsub("er", paste0("er", ix), er_1)
+  er_2 = gsub("er", paste0("er", ix), er_2)
+  
+  parameters = c(pi_1, er_1, er_2)
+  return(parameters)
+}
+
+
+#' func_name()
+#'
+#' Insert description
+#' @param param_1 Insert description
+#' @keywords mrRevBayes
+#' @export
+
+gtr_model_2 = function(ix) {
+  pi_1 = "pi ~ dnDirichlet(v(1,1,1,1))"
+  pi_2 = "moves[++move_index] = mvBetaSimplex(pi, weight=1.0)"
+  er_1 = "er ~ dnDirichlet(v(1,1,1,1,1,1))"
+  er_2 = "moves[++move_index] = mvBetaSimplex(er, weight=1.0)"
+  
+  pi_1 = gsub("pi", paste0("pi_",ix), pi_1)
+  pi_2 = gsub("pi", paste0("pi_",ix), pi_2)
+  er_1 = gsub("er", paste0("er_",ix), er_1)
+  er_2 = gsub("er", paste0("er_",ix), er_2)
+  
+  parameters = c(pi_1, pi_2, er_1, er_2)
+  return(parameters)
+}
+
+#' func_name()
+#'
+#' Insert description
+#' @param param_1 Insert description
+#' @keywords mrRevBayes
+#' @export
+
 # * PARTITIONED ANALYSIS
 partitioned_model = function(scheme_csv, subsets_folder, name) {
   
@@ -82,25 +178,28 @@ partitioned_model = function(scheme_csv, subsets_folder, name) {
       if (same_mods) {
         this_mod = models[this_partition[1]-1]
         
+        if (this_mod == "JC") {
+          script = c(script, (c("\n", gtr_model_2(ix))))
+        } 
+        
         if (this_mod == "GTR") {
-          pi_1 = "pi ~ dnDirichlet(v(1,1,1,1))"
-          pi_2 = "moves[++move_index] = mvBetaSimplex(pi, weight=1.0)"
-          er_1 = "er ~ dnDirichlet(v(1,1,1,1,1,1))"
-          er_2 = "moves[++move_index] = mvBetaSimplex(er, weight=1.0)"
-          
-          pi_1 = gsub("pi", paste0("pi_",ix), pi_1)
-          pi_2 = gsub("pi", paste0("pi_",ix), pi_2)
-          er_1 = gsub("er", paste0("er_",ix), er_1)
-          er_2 = gsub("er", paste0("er_",ix), er_2)
-          script = c(script, (c("\n", pi_1, pi_2, er_1, er_2)))
+          script = c(script, (c("\n", gtr_model_2(ix))))
         }
         
+        if (this_mod == "HKY") {
+          script = c(script, (c("\n", gtr_model_2(ix))))
+        }
+        
+        if (this_mod == "K80") {
+          script = c(script, (c("\n", k80_model_2(ix))))
+        }
+        
+        if (this_mod == "SYM") {
+          script = c(script, (c("\n", sym_model_2(ix))))
+        }
+       
+
       }
-      
-      if (this_mod != "GTR") {
-        print(paste0("Need to write code to accomodate another model."))
-      }
-      
     } 
   }
   
